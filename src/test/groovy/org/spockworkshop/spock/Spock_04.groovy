284d3eb9a3d7b93a365da154abc458862caaf425
pawel.cierpiatka <pawel.cierpiatka@allegro.pl>
2014-01-01 20:49:05 +0100

    adding h2 support

--- src/test/groovy/org/spockworkshop/spock/Spock_04.groovy
+++ src/test/groovy/org/spockworkshop/spock/Spock_04.groovy
@@ -1,5 +1,6 @@
 package org.spockworkshop.spock
 
+import groovy.sql.Sql
 import org.spockworkshop.domain.Account
 import spock.lang.Ignore
 import spock.lang.Shared
@@ -70,16 +71,18 @@ class Spock_04 extends Specification {
     }
 
     @Shared
-    def sql //= Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
+    def sql = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
 
-    @Ignore
+    def setupSpec() {
+        sql.execute("create table maxdata (id int primary key, a int, b int, c int)")
+        sql.execute("insert into maxdata values (1, 3, 7, 7), (2, 5, 4, 5), (3, 9, 9, 9)")
+    }
+
     def "maximum of two numbers using sql "() {
         expect:
-        Math.max(a, b) == c
-
+            Math.max(a, b) == c
         where:
-        [a, b, c] << sql.rows("select a, b, c from maxdata")
-//      [a, b, _, c] << sql.rows("select * from maxdata")
+          [_, a, b, c] << sql.rows("select * from maxdata")
 
     }
 
